7fd743036427be8d62f25e3d14c2b399648cb76f
Emma <81908287+EmmaNaylor@users.noreply.github.com>
2022-01-30 17:49:41 +0000

    Update projectile.py

--- Exercise9/projectile.py
+++ Exercise9/projectile.py
@@ -16,15 +16,3 @@ theta = degree_of_elevation * (pi/180)
 y = barrel_height + distance * tan(theta) - (acceleration * distance ** 2 / (2 * (initial_velocity * cos(theta)) ** 2))
 
 print("The height of the projectile is" + " " + str(y) + " metres.")
-
-
-#
-# - 9.81**2 / 2(math.cos(44)theta)**2
-#
-# # yo
-# # x
-# # tan()
-#
-# theta = 80 * (pi/180)
-# print(theta)
-# print(pi)
